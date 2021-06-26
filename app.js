2d12d3fea8a6ce5e565ecd4d0218a0820c8af05a
luciano <luciano15867@gmail.com>
2021-06-26 18:49:42 -0300

    logica

---
+++ app.js
@@ -0,0 +1,11 @@
+// Declaración de Variables
+let fibo1 = 1;
+let fibo2 = 1;
+
+console.log(`${fibo1}`);
+
+for(let i = 2; i <= 7; i++){
+	console.log(`${fibo2}`);
+	fibo2 = fibo1 + fibo2;
+	fibo1 = fibo2 - fibo1;
+}
